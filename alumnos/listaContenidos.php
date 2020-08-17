f9daefe73b7ac62699d41423648467ce49fe5ccb
Aguedo Meza <joseaguedosernameza@gmail.com>
2020-08-17 06:16:40 -0500

    se acomodan datos mostrados en la tabla de disenador

--- alumnos/listaContenidos.php
+++ alumnos/listaContenidos.php
@@ -9,7 +9,11 @@
     
     $s_idUsuario = $_SESSION["s_IdUser"];
 
-    $qry = "SELECT id, nombre_proyecto, descripcion, fecha_entrega, activo, imagen FROM proyectos where activo = 1 and id_usuario_asignado = '$s_idUsuario'";
+    $qry = "SELECT proyectos.id, proyectos.nombre_proyecto, productos.nombre, activo
+FROM proyectos 
+INNER JOIN productos ON productos.id = proyectos.id_producto
+where activo = 1 
+and id_usuario_asignado = '$s_idUsuario'";
     $consulta = mysqli_query($conexion, $qry);
  ?>
     
@@ -24,9 +28,9 @@
                                     <thead class="thead-dark">
                                         <tr>
                                             <th scope="col">#</th>
-                                            <th scope="col">Nombre</th>
-                                            <th scope="col">Descripcion</th>
-                                            <th scope="col">Entrega</th>
+                                            <th scope="col">Titulo Proyecto</th>
+                                            <th scope="col">Tipo de Proyecto</th>
+                                            
                                            
                                             <th scope="col">Ver contenido</th>
                                         </tr>
@@ -40,7 +44,7 @@
                                             <th scope="row"><?php echo $row[0]; ?></th>
                                             <td><?php echo $row[1]; ?></td>
                                             <td><?php echo $row[2]; ?></td>
-                                            <td><?php echo $row[3]; ?></td>
+                                            
                                             
                                            <td align="center">
                                                <a href="formulario_asignar.php?id=<?php echo $row[0] ?>"><i class="fa fa-file-archive-o fa-2x color-icono" aria-hidden="true"></i></a>
